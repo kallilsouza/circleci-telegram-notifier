8e7c91d6f465e8f12dae4eb516f04976f2e58554
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2024-11-23 14:09:00 -0300

    Fix event loop is closed

--- src/integrations/telegram/client.py
+++ src/integrations/telegram/client.py
@@ -1,8 +1,6 @@
 import asyncio
-
 from telegram import Bot
 from telegram.request import HTTPXRequest
-
 import src.settings as settings
 
 
@@ -12,6 +10,14 @@ class TelegramClient:
         self.bot = Bot(token=settings.TELEGRAM_BOT_TOKEN, request=trequest)
         self.chat_ids = settings.TELEGRAM_CHAT_IDS
 
+    async def _send_message(self, chat_id, message):
+        await self.bot.send_message(chat_id, message, parse_mode="HTML")
+
     def send_pipeline_update(self, message: str) -> None:
-        for chat_id in self.chat_ids:
-            asyncio.run(self.bot.send_message(chat_id, message, parse_mode="HTML"))
+        loop = asyncio.get_event_loop()
+        tasks = [self._send_message(chat_id, message) for chat_id in self.chat_ids]
+
+        if loop.is_running():
+            asyncio.ensure_future(asyncio.gather(*tasks))
+        else:
+            loop.run_until_complete(asyncio.gather(*tasks))
