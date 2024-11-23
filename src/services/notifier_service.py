0309a6796d14acae4b2bc97a9fd9892c490517e1
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2024-11-23 13:28:00 -0300

    Change parse mode to HTML

--- src/services/notifier_service.py
+++ src/services/notifier_service.py
@@ -1,3 +1,5 @@
+from string import Template
+
 from src.integrations.circleci.dataclasses import Event
 from src.integrations.telegram.client import TelegramClient
 
@@ -7,8 +9,15 @@ class NotifierService:
 
     @classmethod
     def notify_users(cls, event: Event) -> None:
-        message = f"""
-            New {event.type} event received.
-        """
+        template = Template(
+            "New <b>${event_type}</b> event received.\n\n"
+            "Project: <b>${project_name}</b>\n"
+            "Organization: <b>${org_name}</b>"
+        )
+        message = template.substitute(
+            event_type=event.type,
+            project_name=event.project.name,
+            org_name=event.organization.name,
+        )
 
         cls.telegram_client.send_pipeline_update(message)
